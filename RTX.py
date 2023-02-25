fc305eda5b51e43eb174ce021e75d99ac631d0ec
cptnsniper <89316950+cptnsniper@users.noreply.github.com>
2023-02-25 12:33:17 -0500

    civics + history class progress

--- RTX.py
+++ RTX.py
@@ -2,9 +2,9 @@ import numpy as np
 from PIL import Image
 import math
 
-x = 160 * 2
-y = 90 * 2
-ray_max = 10000
+x = 160
+y = 90
+ray_max = 100
 aspect = x / y
 
 def dist(vec1, vec2):
@@ -31,17 +31,30 @@ def normal(vec):
 def move_along_vector(vec, dir, dist):
     return vec + normal(dir) * dist
 
-def dist_sphere(point, sphere, radius):
-    return dist(point, sphere) - radius
+def dist_sphere(point, pos, size):
+    return dist(point, pos) - size
 
-def closest_obj(piont, objects):
+def dist_plane(point, pos, size):
+    return point[1] - pos[1]
+
+def closest_obj(point, objects):
     obj = None
     obj_dist = math.inf
     for o in objects:
-        if (dist(piont, o) < obj_dist):
+        cur_dist = o.dist(point)
+        if (cur_dist < obj_dist):
             obj = o
-            obj_dist = dist(piont, o)
-    return obj
+            obj_dist = cur_dist
+    return (obj, obj_dist)
+
+class obj:
+    def __init__(self, position, size, color, dist_func):
+        self.position = np.array(position)
+        self.size = size
+        self.color = color
+        self.func = dist_func
+    def dist(self, point):
+        return self.func(point, self.position, self.size)
 
 def draw(): 
     data = np.zeros((y, x, 3), dtype = np.uint8)
@@ -53,22 +66,33 @@ def draw():
             # u *= -1
             v *= -1
 
-            sphere_origin = np.array([-4, 2, 10])
-            radius = 5
-            sphere_origin1 = np.array([4, -2, 15])
-            radius1 = 7
+            sphere1 = obj([-4, 7, 10], 5, [255, 100, 100], dist_sphere)
+            sphere2 = obj([4, 6, 14], 7, [100, 100, 255], dist_sphere)
+            ground = obj([0, 0, 0], 0, [100, 100, 100], dist_plane)
+            objs = [sphere1, sphere2, ground]
 
-            light = np.array([-5, 10, 0])
+            light = np.array([-8, 12, 2])
+            light_pow = 100
+            world = np.array([20, 20, 40])
+            camera = np.array([0, 5, -2])
+            orthographic = False
+            focal_length = 1
+            camera_dist = 100
 
-            ray = np.array([u * 10, v * 10, 0])
+            ray = camera
+            ray_dir = np.array([ray[0] + u * focal_length, ray[1] + v * focal_length, ray[2] + 1])
+            if (orthographic):
+                ray = ray_dir
+                ray[2] = camera[2]
+                ray_dir = np.array([0, 0, 1 - camera[2]])
             hit = False
             for ii in range(ray_max):
-                ray = move_along_vector(ray, np.array([0, 0, 1]), min(dist_sphere(ray, sphere_origin, radius), dist_sphere(ray, sphere_origin1, radius1)))
-                dist_to_surface = min(dist_sphere(ray, sphere_origin, radius), dist_sphere(ray, sphere_origin1, radius1))
+                ray = move_along_vector(ray, ray_dir - camera, closest_obj(ray, objs)[1])
+                dist_to_surface = closest_obj(ray, objs)[1]
                 if (dist_to_surface < 0.001):
                     hit = True
                     break
-                elif (dist_to_surface > 100):
+                elif (dist_to_surface > camera_dist):
                     break
             
             shadow = False
@@ -76,31 +100,28 @@ def draw():
                 shadow_ray = ray
                 prev_dist = 0
                 for ii in range(ray_max):
-                    shadow_ray = move_along_vector(shadow_ray, light - ray, min(dist_sphere(shadow_ray, sphere_origin, radius), dist_sphere(shadow_ray, sphere_origin1, radius1)))
-                    dist_to_surface = min(dist_sphere(shadow_ray, sphere_origin, radius), dist_sphere(shadow_ray, sphere_origin1, radius1))
+                    shadow_ray = move_along_vector(shadow_ray, light - ray, closest_obj(shadow_ray, objs)[1])
+                    dist_to_surface = closest_obj(shadow_ray, objs)[1]
                     if (dist_to_surface < 0.0001 and prev_dist > dist_to_surface):
                         shadow = True
                         break
-                    elif (dist_to_surface > 100):
+                    elif (dist_to_surface > camera_dist):
                         break
                     prev_dist = dist_to_surface
 
             darkness = 0
-            obj = closest_obj(ray, [sphere_origin, sphere_origin1])
-            if (not shadow):
-                light_pow = 255 * 40 / pow(dist(ray, light), 2)
-                light_angle = angle(light - obj, ray - obj)
-                light_angle = clamp(light_angle / math.pi * 2, 0, 1)
-                darkness = light_pow * (1 - light_angle)
+            cur_obj = closest_obj(ray, objs)[0]
+            if (not False):
+                light_pow = light_pow / pow(dist(ray, light), 2)
+                light_angle = angle(light - cur_obj.position, ray - cur_obj.position)
+                light_angle = clamp(0.5 - (light_angle / math.pi), 0, 1)
+                darkness = light_pow * (light_angle) * 2
+                # darkness = (0.5 - light_angle) * 2
 
             if (hit):
-                if (obj[0] == sphere_origin[0]):
-                    data[j, i] = [clamp(darkness * 2, 20, 255), clamp(darkness, 20, 255), clamp(darkness, 20, 255)]
-                else:
-                    data[j, i] = [clamp(darkness, 20, 255), clamp(darkness * 2, 20, 255), clamp(darkness, 20, 255)]
-                # data[j, i] = [map(angle(light, ray - sphere_origin), 0, math.pi, 255, 0), map(angle(light, ray - sphere_origin), 0, math.pi, 255, 0), map(angle(light, ray - sphere_origin), 0, math.pi, 255, 0)]
+                    data[j, i] = [clamp(cur_obj.color[0] * darkness + world[0] / 255, 0, 255), clamp(cur_obj.color[1] * darkness + world[1] / 255, 0, 255), clamp(cur_obj.color[2] * darkness + world[2] / 255, 0, 255)]
             else:
-                data[j, i] = [20, 20, 40]
+                data[j, i] = world
 
     return data
 
